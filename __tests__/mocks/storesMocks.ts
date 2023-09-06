0ef1d8a1d87439a0b91ce2f2b788a463a4bb2bc1
mottak <kissylamottapereira@gmail.com>
2023-09-06 13:34:22 -0300

    test: atualiza mocks

---
+++ __tests__/mocks/storesMocks.ts
@@ -0,0 +1,39 @@
+export const allStores = [
+  {
+    id: 1,
+    name: "Maria Gourmet",
+    urlLogo: "https://www.prisma.io/nextjs",
+    address: "Rua dos beija-flores, 300 Bairro Planices",
+    ownerId: 1
+  },
+  {
+    id: 2,
+    name: "Maria Costuras",
+    urlLogo: "https://www.prisma.io/nextjs",
+    address: "Rua dos beija-flores, 3500 Bairro Planices",
+    ownerId: 1
+  },
+  {
+    id: 3,
+    name: "Hamburgão do Bob",
+    urlLogo: "https://www.prisma.io/nextjs",
+    address: "Rua das Araras, 430 Bairro Planices",
+    ownerId: 2
+  }
+]
+
+export const newStore = {
+  id: 4,
+  name: "Nova loja do bairro",
+  urlLogo: "https://www.prisma.io/nextjs",
+  address: "Rua das Araras, 450 Bairro Planices",
+  ownerId: 2
+
+}
+
+export const createNewStore =  {
+  name: "Nova loja do bairro",
+  urlLogo: "https://www.prisma.io/nextjs",
+  address: "Rua das Araras, 450 Bairro Planices",
+  ownerId: 2
+}
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -10,9 +10,7 @@ const usersRouter = Router();
 usersRouter.get('/users', userController.list);
 usersRouter.post('/user', usermiddleware.validNewUser, userController.create);
 
-usersRouter.put('/user/name/:id', auth.authUser, usermiddleware.idExists, usermiddleware.validEditName, userController.editName);
-usersRouter.put('/user/email/:id', auth.authUser, usermiddleware.idExists, usermiddleware.validEditEmail, userController.editEmail);
-usersRouter.put('/user/password/:id', auth.authUser, usermiddleware.idExists, usermiddleware.validEditPassword, auth.authUser, userController.editPassword);
+usersRouter.put('/user/name/:id', auth.authUser, usermiddleware.idExists, usermiddleware.validEdit, userController.edit);
 
 usersRouter.delete('/user/:id', userController.remove);
 
